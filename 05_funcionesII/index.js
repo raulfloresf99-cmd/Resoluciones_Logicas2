4b19d72ebe62a3bd582796ef3313440926af8bb1
raulfloresf99-cmd <raulfloresf99@gmail.com>
2025-10-01 15:51:22 -0300

    Add files via upload

---
+++ 05_funcionesII/index.js
@@ -0,0 +1,12 @@
+// Ejercicios del práctico 06 de Funciones 02
+console.log("================================");
+console.log("PRáctico 06 - Funciones 02");
+console.log("================================");
+
+//1. Función nativa de javascript
+function saludar() {
+  console.log("Hola, soy la función saludar");
+}
+
+//Para ejecutar la función, debemos llamarla
+saludar();
